e3176f07d3cb9acf643b4013971231f0cd82fc15
cngrax <384380574@qq.com>
2020-07-24 17:50:29 +0800

    编辑

--- Test2/Test2/Controllers/HomeController.cs
+++ Test2/Test2/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Test2.Controllers
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            _logger = logger; 
         }
 
         public IActionResult Index()
